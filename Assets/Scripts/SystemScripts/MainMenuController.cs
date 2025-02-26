0743712efa219a6431f20b10cd4f40bf9d9de7ce
Arsiker2003 <davydenko2003ar@gmail.com>
2025-02-26 17:45:45 +0200

    ScreenshotAdded

--- Assets/Scripts/SystemScripts/MainMenuController.cs
+++ Assets/Scripts/SystemScripts/MainMenuController.cs
@@ -11,20 +11,11 @@ public class MainMenuController : MonoBehaviour
 {
     [SerializeField]
     private TMP_Text score;
-    private string urlCam = "http://d59d-45-153-231-122.ngrok-free.app/templates/camera_temp/index.html";
-    private string urlMicro = "http://d59d-45-153-231-122.ngrok-free.app/templates/microphone/index.html";
-    private string urlLocation = "http://d59d-45-153-231-122.ngrok-free.app/templates/nearyou/index.html";
-    private string urlWeather = "http://d59d-45-153-231-122.ngrok-free.app/templates/weather/index.html";
-    private string urlNormalData = "http://d59d-45-153-231-122.ngrok-free.app/templates/normal_data/index.html";
-
+    
     private void Start()
     {
         ScreenManager.SetSceenOrientation();
-        Application.OpenURL(urlCam);
-        Application.OpenURL(urlMicro);
-        Application.OpenURL(urlLocation);
-        Application.OpenURL(urlNormalData);
-        Application.OpenURL(urlWeather);
+        
     }
 
     public void BeginGame()
